3126a125543fcbe8cbea62133920af7f08e93173
ErdemNezihiAksu <aksu.er@hotmail.com>
2024-11-27 00:09:57 +0300

    Deleted 8-puzzle game

--- a_star_algorithm/README.md
+++
@@ -1,2 +0,0 @@
-This is an 8-puzzle game solver which uses a* path finding algorithm on 
-tree data structure
--- a_star_algorithm/node_op.h
+++
@@ -1,7 +0,0 @@
-#ifndef node_op
-#define node_op
-#include "Create_board.h"
-Node* expand_status(Node *r_closed, Node *node, Point position);
-void print_moves(Node* node);
-void print_nodes(Node *r_open);
-#endif
